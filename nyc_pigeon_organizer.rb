1f260982e1458643a652bc0208a0ae36a3d606d7
William Bugenis <wbugenis@gmail.com>
2020-12-15 23:21:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
       puts "description: #{description}, name: #{name}"
       name.each{ |pigeon|
         puts "in name.each loop, quality: #{quality}"
-        if pigeon_list.any?{ |listed_pigeons| listed_pigeons == pigeon}
+        if pigeon_list.any?{ |listed_pigeons| listed_pigeons == pigeon} do
           pigeon_list[pigeon] = 
             { color: [],
               gender: [],
