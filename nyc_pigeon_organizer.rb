52ed8f6aec4ecb68642c8383ec7c5c2d89742d47
William Bugenis <wbugenis@gmail.com>
2020-12-15 22:31:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
       puts "description: #{description}, name: #{name}"
       name.each{ |pigeon|
         if pigeon_list.any?(pigeon) == false
-          puts "pigeon list conditiojnal"
+          puts "pigeon list conditional"
           pigeon_list[name] = {}
         end
       }
