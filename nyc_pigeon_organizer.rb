e8991312fc9cae462fce1fc67a6d071793e4d955
William Bugenis <wbugenis@gmail.com>
2020-12-15 23:03:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,7 +16,7 @@ def nyc_pigeon_organizer(data)
               lives: []
             }
         end
-        pigeon_list[pigeon][quality] << description
+        pigeon_list[pigeon][quality] << description.to_S
       }
     }
   }
