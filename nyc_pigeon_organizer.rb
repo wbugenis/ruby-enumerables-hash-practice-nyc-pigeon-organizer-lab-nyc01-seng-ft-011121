265178abe0d19bfa2f8d893217f7aca1d9f475da
William Bugenis <wbugenis@gmail.com>
2020-12-16 00:00:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  
+  data.each{ |quality, descriptor|
+    descriptor.each{ |description, name|
+      name.each{ |pigeon|
+        if !pigeon_list[pigeon]
+          pigeon_list[pigeon] = {}
+        end
+        if !pigeon_list[pigeon][quality]
+          pigeon_list[pigeon][quality] = []
+        end
+        pigeon_list[pigeon][quality] << description.to_s
+      }
+    }
+  }
+  
+  pigeon_list
 end
+
